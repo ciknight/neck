a4943134af314e8c94055f24031cb82ae246af65
ci_knight <ci_knight@msn.cn>
2015-10-08 16:55:09 +0800

    change sleep time

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,28 @@
 public class Main {
-
-
     public static void main(String[] args) {
-        try{
+        try {
             MyThread a = new MyThread();
             a.start();
-        }catch (Exception e){
+        } catch (Exception e) {
             return;
         }
     }
-
 }
 
 
-
 class MyThread extends Thread {
+    private int minute = 1000 * 60;
+
     @Override
-    public void run(){
-        while(true){
+    public void run() {
+        while (true) {
             System.out.println("print!");
+
             Twist twistNeck = new Twist("休息一下");
+
             try {
-                sleep(10000);
-            }catch (Exception e){
+                sleep(60 * minute);
+            } catch (Exception e) {
                 return;
             }
         }
